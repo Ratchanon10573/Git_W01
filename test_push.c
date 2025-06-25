1430a4bf3e4c64c04d208391aaa7b8814d3725c0
Ratchanon10573 <s6812022610573@email.kmutnb.ac.th>
2025-06-25 15:12:30 +0700

    This is my sirst pudh haha

---
+++ test_push.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char name[100];
+    int age;
+
+    printf("Enter your name: ");
+    scanf("%s", name); // หรือใช้ fgets ถ้าต้องการอ่านเว้นวรรค
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Hello, %s! You are %d years old.\n", name, age);
+
+    return 0;
+}
